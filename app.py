b5b7cb0e127a0d03de1beaaf4ae03302d3a5e683
WenYan <105508730+GreenBam@users.noreply.github.com>
2026-03-19 15:09:28 -0700

    Added temperature and updated the look

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from functions import safe_get, safe_get_ranArt
+from functions import safe_get, get_ran
 from keys import vkey
 
 # Create an instance of Flask
@@ -13,8 +13,15 @@ def index():
 @app.route("/results", methods=["GET", "POST"])
 def results_page():
     if request.method == "POST":
-        curCon = safe_get(location=request.form["user_location"])
-        artwork = safe_get_ranArt(con=curCon)
-        return render_template("results.html", curCon=curCon, artwork=artwork)
+        unit=request.form["unit"]
+        curCon = safe_get(location=request.form["user_location"], unit=unit)
+        artwork = get_ran(con=curCon)
+        if unit == "us":
+            degree = "F"
+        elif unit == "metric":
+            degree = "C"
+        else:
+            degree = "K"
+        return render_template("results.html", curCon=curCon, artwork=artwork, degree=degree)
     else:
         return "Wrong HTTP method", 400
--- functions.py
+++ functions.py
@@ -3,14 +3,17 @@ import random as rd
 
 from keys import vkey
 
-args_VC = {"key": vkey, "include": "current"}
+args_VC = {"key": vkey, "unitGroup": "", "include": "current"}
 base_url_VC = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'
 
 args_M = {"hasImages": "true", "q": ""}
 base_url_M = "https://collectionapi.metmuseum.org/public/collection/v1/"
 
 #first + second part of assignment, takes in location
-def safe_get(location):
+def safe_get(location, unit):
+    if " " in location:
+        return None
+    args_VC["unitGroup"] = unit
     url = base_url_VC + location + "/today?" + urllib.parse.urlencode(args_VC)
     try:
         with urllib.request.urlopen(url) as response:
@@ -23,9 +26,11 @@ def safe_get(location):
         return None
     return(json.loads(data))
 
+#Gets the series of object numbers that fall under the search word
 def safe_get_art(con):
-    condition = con['currentConditions']['conditions'].split(" ")
-    args_M["q"] = condition[len(condition) - 1]
+    if con != None:
+        condition = con['currentConditions']['conditions'].split(" ")
+        args_M["q"] = condition[len(condition) - 1]
     url = base_url_M + "/search?" + urllib.parse.urlencode(args_M)
     try:
         with urllib.request.urlopen(url) as response:
@@ -38,20 +43,30 @@ def safe_get_art(con):
         return None
     return (json.loads(data))
 
-def safe_get_ranArt(con):
-    artworks = safe_get_art(con)
-    if artworks != None:
-        objectNum = rd.randint(0, len(artworks["objectIDs"]) - 1)
-        url = base_url_M + "/objects/" + str(artworks["objectIDs"][objectNum])
-        try:
-            with urllib.request.urlopen(url) as response:
-                data = response.read().decode()
-        except urllib.error.HTTPError as e:
-            print('Error retrieving data: HTTP Error, Error code: ', e.code)
+#gets a random artwork from that list
+def safe_get_ranArt(artworks):
+    objectNum = rd.randint(0, len(artworks["objectIDs"]) - 1)
+    url = base_url_M + "/objects/" + str(artworks["objectIDs"][objectNum])
+    try:
+        with urllib.request.urlopen(url) as response:
+            data = response.read().decode()
+    except urllib.error.HTTPError as e:
+        print('Error retrieving data: HTTP Error, Error code: ', e.code)
+        return None
+    except urllib.error.URLError as e:
+        print('Failed to reach server, URL error. Reason: ', e.reason)
+        return None
+    return json.loads(data)
+
+def get_ran(con):
+    if con != None: 
+        artworks = safe_get_art(con)
+        if artworks != None:
+            data = safe_get_ranArt(artworks)
+            while data["primaryImageSmall"] == "":
+                data = safe_get_ranArt(artworks)
+            return data
+        else:
             return None
-        except urllib.error.URLError as e:
-            print('Failed to reach server, URL error. Reason: ', e.reason)
-            return None
-        return json.loads(data)
     else:
         return None
